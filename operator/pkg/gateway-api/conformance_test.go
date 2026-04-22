5589f6e3bf3994c524d4c916037e5b27ed3754df
xtine <xtineskim@gmail.com>
2026-04-22 17:06:46 -0400

    Comment out skipped test
    Signed-off-by: xtine <xtineskim@gmail.com>

--- operator/pkg/gateway-api/conformance_test.go
+++ operator/pkg/gateway-api/conformance_test.go
@@ -74,7 +74,7 @@ func TestConformance(t *testing.T) {
 	}
 	// TODO: Run MeshGRPCRouteWeight once it is deflaked upstream. See
 	//       GH-42456 for details.
-	skipTests = append(skipTests, "MeshGRPCRouteWeight")
+	//skipTests = append(skipTests, "MeshGRPCRouteWeight")
 	//skipTests = append(skipTests, "MeshHTTPRouteMatching")  // same here
 	//skipTests = append(skipTests, "MeshHTTPRouteNamedRule") // same here
 	options.TimeoutConfig.DefaultPollInterval = 3 * time.Second
