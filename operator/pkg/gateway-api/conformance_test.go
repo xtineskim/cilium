3e1df95d406a7854ce459e96b3477f9a6f4d306b
xtine <xtineskim@gmail.com>
2026-04-22 14:02:56 -0400

    gwapi conformance: adding longer polling time
    Signed-off-by: xtine <xtineskim@gmail.com>

--- operator/pkg/gateway-api/conformance_test.go
+++ operator/pkg/gateway-api/conformance_test.go
@@ -75,9 +75,9 @@ func TestConformance(t *testing.T) {
 	// TODO: Run MeshGRPCRouteWeight once it is deflaked upstream. See
 	//       GH-42456 for details.
 	skipTests = append(skipTests, "MeshGRPCRouteWeight")
-	skipTests = append(skipTests, "MeshHTTPRouteMatching")  // same here
+	//skipTests = append(skipTests, "MeshHTTPRouteMatching")  // same here
 	skipTests = append(skipTests, "MeshHTTPRouteNamedRule") // same here
-	options.TimeoutConfig.DefaultPollInterval = 1 * time.Second
+	options.TimeoutConfig.DefaultPollInterval = 3 * time.Second
 	options.UnusableNetworkAddresses = unusableNetworkAddresses
 	options.UsableNetworkAddresses = usableNetworkAddresses
 	options.SkipTests = append(options.SkipTests, skipTests...)
